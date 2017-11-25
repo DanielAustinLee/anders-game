8566981855dcdaac30c5732b909918525240d83d
DanielAustinLee <danielaustinlee@gmail.com>
2017-11-25 16:38:44 -0500

    Render engine stub

---
+++ Game0.0.1/src/game/Systems/Render.java
@@ -0,0 +1,15 @@
+package game.Systems;
+
+import game.Messaging.*;
+
+public class Render extends System  {
+    @Override
+    public boolean canHandle(Message msg) {
+        return msg instanceof DrawMessage;
+    }
+
+    @Override
+    public void handleMessage(Message msg) {
+
+    }
+}
